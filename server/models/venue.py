7a0918f8ea3608a9625537cfcb5440af321e6e32
Sishir Wagle <sishir.wagle512@gmail.com>
2023-08-08 12:26:27 +0545

    Package for a venue

--- server/models/venue.py
+++ server/models/venue.py
@@ -6,8 +6,11 @@ from datetime import datetime
 from fastapi.encoders import jsonable_encoder
 from fastapi import HTTPException, status
 import uuid, shutil, os
+from server.schemas_new.venue import CreatePackageSchema, EditPackageSchema, BookPackageSchema
 
 collection_name= 'Venue'
+package_collection_name= 'Package'
+booking_collection_name= 'PackageBooking'
 
 class Category(BaseModel):
     category: str
@@ -16,8 +19,8 @@ class VenueSchedule(BaseModel):
     title: str = Field(...)
     start_time: str = Field(...)
     end_time: Optional[str] = ''
-    venue: PyObjectId= Field(...)   # User.id
-    artist: PyObjectId= Field(...)  # User.id
+    venue: str= Field(...)   # User.id
+    artist: str= Field(...)  # User.id
 
 
 class PaymentDetails(BaseModel):
@@ -29,19 +32,16 @@ class PaymentDetails(BaseModel):
 
 
 class PackageBooking(BaseModel):
-    package_id: PyObjectId= Field(...)
-    user_id: PyObjectId= Field(...)
-    venue_id: PyObjectId= Field(...)
-    # name: str
-    # email: str
-    phone: str
-    complete: bool
-    payment_details: PaymentDetails
+    package_id: str= Field(...)
+    user_id: str= Field(...)
+    phone: int
+    complete: bool= False
+    payment_details: Optional[PaymentDetails]
     booking_time: datetime
 
 
 class Package(BaseModel):
-    venue_id: PyObjectId= Field(...)
+    venue_id: str= Field(...)
     name: str = Field(...)
     price: int = Field(...)
     date_time: datetime
@@ -118,8 +118,77 @@ async def add_images(db,venue_id, files):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail='No image was added')
 
-def get_venues():
-    pass
+async def add_package(db,package: CreatePackageSchema,user_id):
+    venue= await get_venue_by_userid(db, user_id)
+    print(venue)
+    pkg= Package(
+        venue_id= venue['_id'],
+        name= package.name,
+        price= package.price,
+        date_time= package.date_time,
+        description= package.description,
+    )
+    encoded = jsonable_encoder(pkg)
+    await db[package_collection_name].insert_one(encoded)
+    return {'success': True}
+
+
+async def edit_package(db,package_id,package: EditPackageSchema, user):
+    package = {k: v for k, v in package.dict().items() if v is not None}
+    check= await check_package_belongs_to_venue(db, package_id, user)
+    if not check:
+        raise HTTPException(status_code=404, detail=f"Package not found")
+    if len(package) >= 1:
+
+        update_result = await db[package_collection_name].update_one(
+            {"_id": package_id}, {"$set": package}
+        )
+
+        if update_result.modified_count == 1:
+            if (
+                updated_package := await db[package_collection_name].find_one({"_id": package_id})
+            ) is not None:
+                return updated_package
+
+    if (
+        existing_package := await db[package_collection_name].find_one({"_id": package_id})
+    ) is not None:
+        return existing_package
+
+    raise HTTPException(status_code=404, detail=f"Package with id {package_id} not found")
+
+async def check_package_belongs_to_venue(db,package_id, user_id):
+    package= await db[package_collection_name].find_one({"_id": package_id})
+    if package is None:
+        raise HTTPException(status_code=404, detail=f"Package not found")
+    venue=await get_venue_by_userid(db,user_id)
+    return package['venue_id']== venue['_id']
+
+async def delete_package(db, package_id, user):
+    check= await check_package_belongs_to_venue(db, package_id, user)
+    if not check:
+        raise HTTPException(status_code=404, detail=f"Package not found")
+    package=await db[package_collection_name].delete_one({'_id': package_id})
+    if package.deleted_count == 1:
+        return {f"Successfully deleted package"}
+    else:
+        raise HTTPException(status_code=404, detail=f"Package not found")
+
+async def book_package(db,package_id,user,booking: BookPackageSchema):
+    package= await db[package_collection_name].find_one({"_id": package_id})
+    if package is None:
+        raise HTTPException(status_code=404, detail=f"Package not found")
+    book= PackageBooking(
+        package_id= package_id,
+        user_id= user,
+        phone= booking.phone,
+        booking_time= datetime.now()
+    )
+    encoded = jsonable_encoder(book)
+    await db[booking_collection_name].insert_one(encoded)
+    return {'success': True}
+
+
 
 
 def edit_venue():
@@ -155,13 +224,7 @@ def validate_venue():
 def invalidate_package():
     pass
 
-def delete_package():
-    pass
-
 def delete_schedule():
-    pass
-
-def book_package():
     pass
 
 def complete_booked_package():
